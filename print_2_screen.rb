b18036581ec758f8ba9b6de8a34b09782263e66d
JBrow95 <browningjustin95@gmail.com>
2018-06-06 14:35:05 -0400

    Added comments to the code

--- print_2_screen.rb
+++ print_2_screen.rb
@@ -12,20 +12,33 @@
 #  num = num + 1
 # end
 
+
+#Variable that equals 1
 num = 1
 
+#loop that writes what is nested in it 100 times
 100.times do
 	
+	#conditional that replaces every number that 5 and 3 go into with minedminds
+
 	if num %5 == 0 and num %3 == 0
 		puts "minedminds"
+
+	#conditional that replaces every number that 3 goes into with mind
 
 	elsif num %3 == 0
 		puts "mind"
 
+	#conditional that replaces every number that 5 goes into with mines
+
 	elsif num %5 == 0
-		puts "mines"					
+		puts "mines"	
+
+	#conditional that prints the variable num to every number that 5 and 3 don't go into
 	else
 		puts num
 	end
+
+	#variable that add 1 everytime it is written
 	num = num + 1
 end
