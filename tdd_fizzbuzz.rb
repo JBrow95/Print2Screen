4c22be132ed088bdb575b982e2cf28ead14834e7
JBrow95 <browningjustin95@gmail.com>
2018-06-19 13:31:44 -0400

    made loop to print array

--- tdd_fizzbuzz.rb
+++ tdd_fizzbuzz.rb
@@ -3,13 +3,26 @@
 
 
 
-fizz_arr = [*1..100]
-fizz_arr[2] = "mined"
-fizz_arr[4] = "mind"
-fizz_arr[14] = "minedminds"
 
-# test begin here
+fizz_arr = [*1..100]
+
+def main(fizz_arr)
+	new_arr = []
+	fizz_arr.each do |n|
+		if n %3 == 0 && n %5 == 0
+			n = "minedminds"
+			# puts n
+		end
+		new_arr << n
+	end
+	# puts new_arr
+	fizz_arr = new_arr
+end
+# main(fizz_arr)
+
+# # test begin here
 def test_length_of_array_is_100(fizz_arr)
+	main(fizz_arr)
 	if fizz_arr.length == 100
 		puts true
 	else
@@ -17,23 +30,25 @@ def test_length_of_array_is_100(fizz_arr)
 	end
 end
 
-def test_third_element_is_mined(fizz_arr)
-	if fizz_arr[2] == "mined"
-		puts true
-	else 
-		puts false
-	end
-end
+# def test_third_element_is_mined(fizz_arr)
+# 	if fizz_arr[2] == "mined"
+# 		puts true
+# 	else 
+# 		puts false
+# 	end
+# end
 
-def test_fifth_element_is_mind(fizz_arr)
-	if fizz_arr[4] == "mind"
-		puts true
-	else
-		puts false
-	end
-end
+# def test_fifth_element_is_mind(fizz_arr)
+# 	if fizz_arr[4] == "mind"
+# 		puts true
+# 	else
+# 		puts false
+# 	end
+# end
 
 def test_fifteenth_elemt_is_minedminds(fizz_arr)
+	fizz_arr = main(fizz_arr)
+	p fizz_arr
 	if fizz_arr[14] == "minedminds"
 		puts true
 	else
@@ -42,6 +57,7 @@ def test_fifteenth_elemt_is_minedminds(fizz_arr)
 end
 
 def test_if_1_is_2(fizz_arr)
+	main(fizz_arr)
 	if fizz_arr[1] == 2
 		puts true
 	else
@@ -50,7 +66,7 @@ def test_if_1_is_2(fizz_arr)
 end
 
 test_length_of_array_is_100(fizz_arr)
-test_third_element_is_mined(fizz_arr)
-test_fifth_element_is_mind(fizz_arr)
+# test_third_element_is_mined(fizz_arr)
+# test_fifth_element_is_mind(fizz_arr)
 test_fifteenth_elemt_is_minedminds(fizz_arr)
 test_if_1_is_2(fizz_arr)
