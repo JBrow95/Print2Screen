e022856196f696df3982b38a14f2adbde0f6a942
JBrow95 <browningjustin95@gmail.com>
2018-06-19 11:06:28 -0400

    test if fifth element in array equals mind

--- tdd_fizzbuzz.rb
+++ tdd_fizzbuzz.rb
@@ -4,11 +4,10 @@
 
 
 fizz_arr = [*1..100]
-
-
+fizz_arr[2] = "mined"
+fizz_arr[4] = "mind"
 
 # test begin here
-
 def test_length_of_array_is_100(fizz_arr)
 	if fizz_arr.length == 100
 		puts true
@@ -25,5 +24,13 @@ def test_third_element_is_mined(fizz_arr)
 	end
 end
 
+def test_fifth_element_is_mind(fizz_arr)
+	if fizz_arr[4] == "mind"
+		puts true
+	else
+		puts false
+	end
+end
 test_length_of_array_is_100(fizz_arr)
 test_third_element_is_mined(fizz_arr)
+test_fifth_element_is_mind(fizz_arr)
