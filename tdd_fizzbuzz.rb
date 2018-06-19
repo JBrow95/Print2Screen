d4f700cec068c5d183007ce5bef4cf5cd8a0a608
JBrow95 <browningjustin95@gmail.com>
2018-06-19 10:32:13 -0400

    update

---
+++ tdd_fizzbuzz.rb
@@ -0,0 +1,19 @@
+
+
+fizz_arr = []
+
+
+
+
+
+
+# test begin here
+
+def test_length_of_array_is_100(fizz_arr)
+	if fizz_arr.length == 100
+		puts true
+	else
+		puts false
+	end
+end
+test_length_of_array_is_100(fizz_arr)
