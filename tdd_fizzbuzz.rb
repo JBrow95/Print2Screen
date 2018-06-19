25bb38e9d0507eb59ddca4f8accdb3250eb4d39a
JBrow95 <browningjustin95@gmail.com>
2018-06-19 11:21:26 -0400

    test array if pos 1 eqauls int 2

--- tdd_fizzbuzz.rb
+++ tdd_fizzbuzz.rb
@@ -6,6 +6,7 @@
 fizz_arr = [*1..100]
 fizz_arr[2] = "mined"
 fizz_arr[4] = "mind"
+fizz_arr[14] = "minedminds"
 
 # test begin here
 def test_length_of_array_is_100(fizz_arr)
@@ -39,7 +40,17 @@ def test_fifteenth_elemt_is_minedminds(fizz_arr)
 		puts false
 	end
 end
+
+def test_if_1_is_2(fizz_arr)
+	if fizz_arr[1] == 2
+		puts true
+	else
+		false
+	end
+end
+
 test_length_of_array_is_100(fizz_arr)
 test_third_element_is_mined(fizz_arr)
 test_fifth_element_is_mind(fizz_arr)
 test_fifteenth_elemt_is_minedminds(fizz_arr)
+test_if_1_is_2(fizz_arr)
